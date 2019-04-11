c1447b3cec084d0ccaabf39dc2ba5526cca8f3e8
Gordon Martin <gordon.hugh.martin@gmail.com>
2019-04-11 17:51:59 +0100

    Helper index plugin for quickly querying akka persistence IDs in the system.

---
+++ index.js
@@ -0,0 +1,52 @@
+const pull = require('pull-stream');
+const FlumeReduce = require('flumeview-reduce');
+
+exports.name = 'akka-persistence-index'
+exports.version = require('./package.json').version
+
+exports.manifest = {
+    currentPersistenceIds: 'source',
+    livePersistenceIds: 'source'
+}
+
+const indexVersion = 1;
+
+exports.init = (ssb, config) => {
+    const view = ssb._flumeUse('akka-persistence-index',
+        FlumeReduce(
+        indexVersion,
+        flumeReduceFunction,
+        flumeMapFunction)
+    )
+
+    function flumeReduceFunction(index, persistenceId) {
+
+        if (!index) index = []
+
+        if (!index.includes(persistenceId)) {
+            index.push(persistenceId)
+        }
+
+        return index;
+    }
+
+    function flumeMapFunction(item) {
+        // If this is a akka persistence type message, we return it - if there isn't, the reducer isn't ran for 'undefined'
+        // values
+        return item.value.content['persistenceId'];
+    }
+
+    return {
+        currentPersistenceIds: () => {
+            return pull(view.stream({live: false}), pull.flatten())
+        },
+        livePersistenceIds: () => {
+            return pull(view.stream({live: true}), pull.flatten(), pull.unique(), pull.filter(item => !item.sync))
+        }
+    }
+
+}
+
+
+
+
