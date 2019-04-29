faffb3698ec1e1ff39e4f39800ffcf4836d20c52
Gordon Martin <gordon.hugh.martin@gmail.com>
2019-04-29 18:56:09 +0100

    Fix class name

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
 module.exports.setKeyType = "org.openlaw.scuttlebutt.persistence.model.UpdateKey";
-module.exports.addUserType = "org.openlaw.scuttlebutt.persistence.model.AllowKey";
+module.exports.addUserType = "org.openlaw.scuttlebutt.persistence.model.AllowAccess";
 module.exports.removeUserType = "org.openlaw.scuttlebutt.persistence.model.RemoveAccess";
